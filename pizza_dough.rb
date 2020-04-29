e6001b87406f48ee82b2d543140495726a5a3829
Phaedon Val <faival@protonmail.com>
2020-04-30 00:08:53 +0300

    playing 'round

---
+++ pizza_dough.rb
@@ -0,0 +1,9 @@
+# It simly finds for (f) flour how much water you will need to make the perfect pizza dough!!!
+
+def water_for_pizza_dough(flo)
+  water = f * 0.66667
+  puts "You used #{flo}gr of flour, so you need #{
+    water}ml of water!"
+end
+
+water_for_pizza_dough(618)
